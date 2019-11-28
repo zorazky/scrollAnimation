d2b96616c3fbbfde98d562c72b16bd40732dc833
zorazky <oswaldo.vazquez@gmail.com>
2019-11-28 13:49:57 -0600

    finished

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var tl = new TimelineMax({onUpdate:updatePercentage});
+var tl2 = new TimelineMax();
 const controller = new ScrollMagic.Controller();
 
 tl.from("blockquote", .5, {x:200, opacity: 0});
-tl.from("#office", 1, {x:-200, opacity: 0});
-tl.from("#building", 1, {x:200, opacity:0});
+tl.from("span", 1, {width:0}, "=-.5")
+tl.from("#office", 1, {x:-200, opacity: 0}, "=-1");
+tl.from("#building", 1, {x:200, opacity:0}, "=-.7");
+
+tl2.from("#box", 1, {opacity:0, scale:0})
+tl2.to("#box", .5, {
+    left:"20%",
+    scale: 1.3,
+    borderColor: 'white',
+    borderWidth: 12,
+    boxShadow: '1px 1px 0px 0px rgba(0,0,0,.09)'
+})
 
 const scene = new ScrollMagic.Scene({
     triggerElement: ".sticky",
@@ -13,6 +24,13 @@ const scene = new ScrollMagic.Scene({
 
 .setPin(".sticky")
 .setTween(tl)
+.addTo(controller);
+
+const scene2 = new ScrollMagic.Scene({
+    triggerElement: "blockquote"
+})
+
+.setTween(tl2)
 .addTo(controller);
 
 function updatePercentage() {
